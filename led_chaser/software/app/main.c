926ebc4d4bb8ae5c02f35714214448b4734dde52
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-03 22:36:59 +0100

    Modification main.c pour allumer éteindre des leds

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -1,7 +1,13 @@
-#include "sys/alt_stdio.h"
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
 
 int main(){
 	
-	alt_printf("Hello World\n");
 	
+	while(1){
+	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
+	usleep(400000);
+	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x10);
+	usleep(400000);
+	}
 }
