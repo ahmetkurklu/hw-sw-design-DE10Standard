e465f5f84924b5b2390304ab7e84b6f2e7b8930e
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-03 23:01:45 +0100

    Fin version 1 du led_chaser avec un while

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -3,11 +3,21 @@
 
 int main(){
 	
+	int i = 0;
+	int addr = 0x01;
 	
-	while(1){
 	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 	usleep(400000);
-	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x10);
-	usleep(400000);
+	while(1){
+		while(addr != 0x80){
+			addr = addr<<1;
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
+			usleep(400000);
+		}
+		while(addr != 0x01){
+			addr = addr>>1;
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
+			usleep(400000);
+		}
 	}
 }
