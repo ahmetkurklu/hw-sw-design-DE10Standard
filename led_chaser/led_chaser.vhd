6e389f1303192de3577046fffe4094e9218aa80e
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-03 23:50:46 +0100

    Ajout POI pour les keys dans le .qsys, ajout des keys dans le .vhd , ajout des pin et modification du .c pour modifier la vitesse en fonction du key presser

--- led_chaser/led_chaser.vhd
+++ led_chaser/led_chaser.vhd
@@ -7,6 +7,7 @@ entity led_chaser is
 	port(
 		clk : in std_logic := '0';
 		leds : out std_logic_vector(7 downto 0);
+		keys : in std_logic_vector(3 downto 0);
 		reset_n : in std_logic :='0');
 end entity led_chaser;
 
@@ -15,6 +16,7 @@ architecture rit of led_chaser is
         port (
             clk_clk       : in std_logic := 'X'; -- clk
 				pio_0_external_connection_export : out std_logic_vector(7 downto 0);
+				pio_1_external_connection_export : in  std_logic_vector(3 downto 0) := (others => 'X');
             reset_reset_n : in std_logic := 'X'  -- reset_n
         );
     end component led_chaser_sys;
@@ -24,6 +26,7 @@ begin
         port map (
             clk_clk       => clk,       --   clk.clk
 				pio_0_external_connection_export => leds,
+				pio_1_external_connection_export => keys,
             reset_reset_n => reset_n  -- reset.reset_n
         );
 		  
--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -1,23 +1,44 @@
 #include "system.h"
 #include "altera_avalon_pio_regs.h"
+#include "sys/alt_stdio.h"
 
 int main(){
 	
-	int i = 0;
 	int addr = 0x01;
+	int time = 400000;
+	int key;
 	
 	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
-	usleep(400000);
+	usleep(time);
 	while(1){
+		key = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
+		alt_printf("%x\n",key);
+		if(key == 0x0E){
+			time = 200000;
+		}
+		else if(key == 0x0D){
+			time = 100000;
+		}
+		else if(key == 0x0B){
+			time = 50000;
+		}
+		else if(key == 0x07){
+			time = 25000;
+		}
+		else{
+			time = 400000;
+		}
+		
 		while(addr != 0x80){
 			addr = addr<<1;
 			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
-			usleep(400000);
+			usleep(time);
 		}
 		while(addr != 0x01){
 			addr = addr>>1;
 			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
-			usleep(400000);
+			usleep(time);
 		}
 	}
 }
+
