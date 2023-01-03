96e26a212fea88eeb7fc46188e5a0d789dfc29b1
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-03 21:41:50 +0100

    modification fichier vhdl pour ajouter les leds et modification fichier qsys avec ajout de PIO

--- led_chaser/led_chaser.vhd
+++ led_chaser/led_chaser.vhd
@@ -6,6 +6,7 @@ use ieee.numeric_std.all;
 entity led_chaser is
 	port(
 		clk : in std_logic := '0';
+		leds : out std_logic_vector(7 downto 0);
 		reset_n : in std_logic :='0');
 end entity led_chaser;
 
@@ -13,6 +14,7 @@ architecture rit of led_chaser is
 	component led_chaser_sys is
         port (
             clk_clk       : in std_logic := 'X'; -- clk
+				pio_0_external_connection_export : out std_logic_vector(7 downto 0);
             reset_reset_n : in std_logic := 'X'  -- reset_n
         );
     end component led_chaser_sys;
@@ -21,6 +23,7 @@ begin
 	u0 : component led_chaser_sys
         port map (
             clk_clk       => clk,       --   clk.clk
+				pio_0_external_connection_export => leds,
             reset_reset_n => reset_n  -- reset.reset_n
         );
 		  
