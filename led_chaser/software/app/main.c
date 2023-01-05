b1dab2085946c449b52e824345d7f37469412102
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-05 15:51:04 +0100

    Test exemple d'interruption doc intel non fonctionnel

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -6,26 +6,55 @@
 #include "io.h"
 #include "unistd.h"
 
-static void irqhandler(void * context, alt_u32 id){
-	alt_printf("Dans main\n");
+
+
+
+volatile int edge_capture;
+
+static void handle_button_interrupts(void* context){
+	volatile int* edge_capture_ptr = (volatile int*) context;
+	*edge_capture_ptr = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE);
+	alt_printf("Dans interrupt\n");
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0);
+	IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE);
 }
+
+
 
 int main(){
 	
-	alt_printf("Dans main\n");
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_IRQ, 0x0F);
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_IRQ, 0x00);
-	alt_irq_register(PIO_1_IRQ,NULL, (void*)irqhandler);
+	
+	
+	
+	
+	
+	
+	alt_printf("Dans le main\n");
+	
+	void* edge_capture_ptr = (void*) &edge_capture;
+	
+	
+	//IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE, 1);
+	//Reset the edge capture register. 
+	//IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0);
+	
+	if(alt_ic_isr_register(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID, PIO_1_IRQ, handle_button_interrupts, edge_capture_ptr, 0) != 0){
+		alt_printf("ERROR \n");
+	}
+	if(alt_ic_irq_enable(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ) != 0){
+		alt_printf("interrupt enable error\n");
+	}
+	if(alt_ic_irq_enabled(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ) != 0){
+		alt_printf("interrupt enabled\n");
+	}
+	
+	
+	alt_printf("fin init\n");	
 	while(1){
 		
 	}
-	
 	return 0;
 }
-
-
-
-
 
 
 /*
@@ -34,7 +63,7 @@ int main(){
 	int addr = 0x01;
 	int time = 400000;
 	int key;
-	
+	alt_printf("Dans le main\n");
 	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 	usleep(time);
 	while(1){
@@ -68,5 +97,4 @@ int main(){
 		}
 	}
 }
-
 */
