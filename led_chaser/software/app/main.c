7d465dd71aba11aefaac0938367dee05ce46b588
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-07 00:32:21 +0100

    Fin implémentation avec interrupt

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -9,6 +9,8 @@
 
 static void key_interrupt(void *Context);
 int time = 400000;
+int key;
+
 
 int main(){
 	int mask;
@@ -17,11 +19,13 @@ int main(){
 	alt_printf("In main start\n");
 	
 	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
-	alt_printf("ADDR = %x\n",mask);
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE,0x1);
+	alt_printf("MASK = %x\n",mask);
+	
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE,0xf);
 	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
-	alt_printf("ADDR = %x\n",mask);
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0x1);
+	alt_printf("MASK = %x\n",mask);
+	
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0xf);
 	
 	if(alt_ic_isr_register(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ,key_interrupt,NULL,NULL) != 0){
 		alt_printf("Erreur creation interruption\n");
@@ -44,9 +48,29 @@ int main(){
 }
 
 static void key_interrupt(void *Context){
+	
 	alt_printf("INTERRUPT\n");
 	time = time/2;
+	
+	key = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
+		alt_printf("%x\n",key);
+		switch(key){
+			case 0x07:
+				time = 500000;
+				break;
+			case 0x0b:
+				time = 200000;
+				break;
+			case 0x0d:
+				time = 100000;
+				break;
+			case 0x0e:
+				time = 20000;
+				break;
+			default:
+				time = 400000;
+				break;
+		}
 
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0x1);
-	IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0xf);
 }
