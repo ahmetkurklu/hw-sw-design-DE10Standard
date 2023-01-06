af44785361fb0aeb9c68fe8e4e4f2e4f32b84a5f
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-06 23:06:33 +0100

    Modification de la vitesse du led_chaser avec une interruption un bouton

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -8,33 +8,44 @@
 
 
 static void key_interrupt(void *Context);
-
-
-
+int time = 400000;
 
 int main(){
-	int addr;
+	int mask;
+	int addr = 0x01;
+	
 	alt_printf("In main start\n");
 	
-	addr = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
-	alt_printf("ADDR = %x\n",addr);
+	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
+	alt_printf("ADDR = %x\n",mask);
 	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE,0x1);
-	addr = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
-	alt_printf("ADDR = %x\n",addr);
+	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
+	alt_printf("ADDR = %x\n",mask);
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0x1);
 	
 	if(alt_ic_isr_register(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ,key_interrupt,NULL,NULL) != 0){
 		alt_printf("Erreur creation interruption\n");
 	}
 	
+	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
+	usleep(time);
 	while(1){
-		
+		while(addr != 0x80){
+			addr = addr<<1;
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
+			usleep(time);
+		}
+		while(addr != 0x01){
+			addr = addr>>1;
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,addr);
+			usleep(time);
+		}
 	}
 }
 
 static void key_interrupt(void *Context){
 	alt_printf("INTERRUPT\n");
-
+	time = time/2;
 
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0x1);
 	IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE);
