8d64319797a7a2236cd6f59ec7facf94daaef35b
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-07 00:36:39 +0100

    Ajout commentaires

--- led_chaser/software/app/main.c
+++ led_chaser/software/app/main.c
@@ -6,31 +6,39 @@
 #include "io.h"
 #include "unistd.h"
 
-
+//Initialisation fonction handler interrupt
 static void key_interrupt(void *Context);
+//Variable pour le temps d'att afin de gere la vitesse de led_chaser
 int time = 400000;
+//Variable pour recupere le bouton presser
 int key;
 
 
 int main(){
+	// Variable pour debug
 	int mask;
 	int addr = 0x01;
 	
 	alt_printf("In main start\n");
 	
+	
 	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
 	alt_printf("MASK = %x\n",mask);
 	
+	// applique un mask 0b1111 afin d'activer les boutons
 	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE,0xf);
 	mask = IORD_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE);
 	alt_printf("MASK = %x\n",mask);
 	
+	// active la detection des boutons
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0xf);
 	
+	//Création de l'interruption
 	if(alt_ic_isr_register(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ,key_interrupt,NULL,NULL) != 0){
 		alt_printf("Erreur creation interruption\n");
 	}
 	
+	//Led chaser
 	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 	usleep(time);
 	while(1){
@@ -51,7 +59,7 @@ static void key_interrupt(void *Context){
 	
 	alt_printf("INTERRUPT\n");
 	time = time/2;
-	
+	// On vérifier le bouton et on modifier la valeur du temps
 	key = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
 		alt_printf("%x\n",key);
 		switch(key){
@@ -71,6 +79,6 @@ static void key_interrupt(void *Context){
 				time = 400000;
 				break;
 		}
-
+	//Reset la detection des boutons
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE,0xf);
 }
