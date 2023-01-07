61a2d8bc8aa70af973281e74732469af6094cc1e
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-07 23:08:11 +0100

    fin configuration hello world soft validé

---
+++ lets_count_stuff/software/app/main.c
@@ -0,0 +1,12 @@
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include "sys/alt_stdio.h"
+#include "sys/alt_irq.h"
+#include "alt_types.h"
+#include "io.h"
+#include "unistd.h"
+
+
+int main(){
+	alt_printf("Hello World\n");
+}
