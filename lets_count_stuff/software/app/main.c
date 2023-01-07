33ed945c4e8b15131fd0a5b4a6a9ae85f93bc898
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-07 23:54:16 +0100

    main.c du compteur 0 a 9 validé

--- lets_count_stuff/software/app/main.c
+++ lets_count_stuff/software/app/main.c
@@ -7,6 +7,67 @@
 #include "unistd.h"
 
 
+
+void segment(int addr,int nbr){
+	
+	switch(nbr){
+		case 0: 
+			alt_printf("0\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b1000000);
+			break;
+		case 1: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b1111001);
+			break;
+		case 2: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0100100);
+			break;
+		case 3: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0110000);
+			break;
+		case 4: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0011001);
+			break;
+		case 5: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0010010);
+			break;
+		case 6: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0000010);
+			break;
+		case 7: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b1111000);
+			break;
+		case 8: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0000000);
+			break;
+		case 9: 
+			alt_printf("1\n");
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b0010000);
+			break;
+		default:
+			IOWR_ALTERA_AVALON_PIO_DATA(addr,0b1111111);
+			break;
+	}
+	usleep(400000);
+}
+
+
+
 int main(){
-	alt_printf("Hello World\n");
+	alt_printf("Dans le main\n");
+	
+	int i =0;
+	while(1){
+		for(i=0;i<10;i++){
+			segment(PIO_0_BASE,i);
+		}
+		i = 0;
+	}
 }
